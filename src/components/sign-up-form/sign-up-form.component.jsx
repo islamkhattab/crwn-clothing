a3266599c78b5b3551159f3e31bd98b855480f38
Islam Khattab <islam.n.khattab@gmail.com>
2023-08-27 09:45:10 -0400

    Adding user context to store user state

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -8,6 +8,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import "./sign-up-form.styles.scss";
+
+import { UserContext } from "../../contexts/user.context";
 
 const defaultFormFields = {
   displayName: "",
@@ -19,6 +21,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+
+  const { setCurrectUser } = useContext(UserContext);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -49,6 +53,9 @@ const SignUpForm = () => {
       );
       await createUserDocumentFromAuth(user, { displayName });
       alert("User created!");
+
+      setCurrectUser(user);
+      
       resetFormFields();
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
