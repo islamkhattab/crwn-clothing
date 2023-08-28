a53d3bf1ddc03ccfa189f676255b6c82582ec2a7
Islam Khattab <islam.n.khattab@gmail.com>
2023-08-27 21:41:11 -0400

    Adding cart counter to panel and cart items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,15 +6,14 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  
-    const { IsCartOpen, setIsCartOpen} = useContext(CartContext);
+  const { IsCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-    const handleCartIconClick = (event)=> setIsCartOpen(!IsCartOpen);
-    
-    return (
-    <div className='cart-icon-container' onClick={handleCartIconClick}>
-      <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+  const handleCartIconClick = (event) => setIsCartOpen(!IsCartOpen);
+
+  return (
+    <div className="cart-icon-container" onClick={handleCartIconClick}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
--- src/components/cart-panel/cart-panel.component.jsx
+++ src/components/cart-panel/cart-panel.component.jsx
@@ -1,12 +1,27 @@
-import Button from '../button/button.component';
+import { useContext } from "react";
 
-import './cart-panel.styles.scss'
+import { CartContext } from "../../contexts/cart.context";
 
-const CartPanel = () => (
-  <div className='cart-dropdown-container'>
-    <div className='cart-items' />
-    <Button>GO TO CHECKOUT</Button>
-  </div>
-);
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-panel.styles.scss";
+
+const CartPanel = () => {
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => <CartItem key={cartItem.id} cartItem={cartItem} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartPanel;
